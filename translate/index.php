4e3d9ff207405416df37b0cee421e6848880dc17
Fernando Bueno <buenofernando13@gmail.com>
2020-08-03 18:23:07 -0300

    Create index.php
    Website translation system in PHP, by user interaction

---
+++ translate/index.php
@@ -0,0 +1,16 @@
+<?php include('translator.php');?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title><?php echo $trad['titulo'];?></title>
+	</head>
+	
+	<body>
+	<a href="?language=pt-br.php">Português</a>
+	<a href="?language=en-us.php">English</a>
+	<a href="?language=es-es.php">Español</a>
+	<?php echo $trad['titulo'];?>
+		
+	</body>
+</html>
